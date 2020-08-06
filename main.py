6f48e901c2aac833b8b7ce0f79473ca114d9632e
wan <wodhksqw@naver.com>
2020-08-07 00:54:45 +0900

    db연동 추가

--- main.py
+++ main.py
@@ -3,20 +3,23 @@ import sys
 from PyQt5.QtWidgets import *
 from datetime import datetime
 import time
+from DbControl import *
 
 app = QApplication(sys.argv)
 k = KiwoomS()
+db = DbCon("kospi.db")
 k._login()
 
 k._set_opt10081("024060", "20200805", 0)
 print(k.ohlc)
 print(k.nextValueHave)
 time.sleep(0.5)
-
 print(type(k.nextValueHave))
 
 while k.nextValueHave == '2':
     print("진입")
+    db.save_dataframe_to_db(k.ohlc, tableName="024060")
     k._set_opt10081("024060", "20200805", 2)
     print(k.ohlc)
+
     time.sleep(0.5)
