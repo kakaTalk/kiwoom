acc6836f578c41506d276f7549193e2803a38fc4
wan <wodhksqw@naver.com>
2020-08-08 18:42:07 +0900

    추가한 부분 테스트 코드 반영

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
 from Kiwoom import *
 import sys
 from PyQt5.QtWidgets import *
-from datetime import datetime
-import time
 from DbControl import *
+from UIControl import MyWindow
 
-app = QApplication(sys.argv)
-k = KiwoomS()
-db = DbCon("kospi.db")
-k._login()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = MyWindow()
+    myWindow.show()
+    app.exec_()
 
-k._set_opt10081("024060", "20200805", 0)
-print(k.ohlc)
-print(k.nextValueHave)
-time.sleep(0.5)
-print(type(k.nextValueHave))
-
-while k.nextValueHave == '2':
-    print("진입")
-    db.save_dataframe_to_db(k.ohlc, tableName="024060")
-    k._set_opt10081("024060", "20200805", 2)
-    print(k.ohlc)
-
-    time.sleep(0.5)
