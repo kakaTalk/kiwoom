b687efda08affcaa731de3714ed94d35d9e2e9b0
wan <wodhksqw@naver.com>
2020-08-14 01:33:10 +0900

    그래프를 ui안에서 뛰우는걸로 변경

--- Draw.py
+++ Draw.py
@@ -1,26 +1,31 @@
 
 import matplotlib.pyplot as plt
 import mpl_finance
-import matplotlib.ticker
-import datetime
+from  matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 #db그리기 테스트임
 
 from  DbControl import DbCon
 
 class Draw:
-    def __init__(self):
+    def __init__(self, pannel):
         self.fig = plt.figure(figsize=(12, 8))
+
+        self.pannel = pannel
+        self.canvas = FigureCanvas(self.fig)
+        self.pannel.addWidget(self.canvas)
+
         self.make_subplot()
 
     def make_subplot(self):
-        self.topAxax = plt.subplot2grid((4,4), (0,0), rowspan=3, colspan=4)            #sub플롯의 크기를 조절 가능
-        self.bottomAxax = plt.subplot2grid((4, 4), (3, 0), rowspan=1, colspan=4)
+        self.topAxax = self.fig.add_subplot(111)            #sub플롯의 크기를 조절 가능
+        # self.bottomAxax = self.fig.subplot2grid((4, 4), (3, 0), rowspan=1, colspan=4)
 
-    def draw_ohlc(self, data):
-        mpl_finance.candlestick2_ohlc(self.topAxax, data['open'], data['high'], data['low'], data['close'], width=0.5, colorup='r', colordown='b')
-        plt.draw()
-        plt.show()
+    def draw_ohlc(self, data):  #data의 변동이 있을때만 다시 그리기
+        self.topAxax.cla()
+        mpl_finance.candlestick2_ohlc(self.topAxax, data['open'][-60:], data['high'][-60:], data['low'][-60:], data['close'][-60:], width=0.5, colorup='r', colordown='b')
+        self.canvas.draw()
+
 
 
 
